2f53bf854de2d63cc92018f4adcdfe336ec4e932
skydeszka <60075235+Skydeszka@users.noreply.github.com>
2022-05-24 23:31:31 +0200

    Create BotContext.cs

---
+++ Data/BotContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.Sqlite;
+using TestBot.Models;
+
+namespace TestBot.Data;
+
+public class BotContext : DbContext
+{
+    public DbSet<User> Users => Set<User>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionStringBuilder = new SqliteConnectionStringBuilder { DataSource = Program.DBPath };
+        var connectionString = connectionStringBuilder.ToString();
+        var connection = new SqliteConnection(connectionString);
+        optionsBuilder.UseSqlite(connection);
+    }
+}
