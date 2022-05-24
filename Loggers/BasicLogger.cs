97a93769e04dfed9cd16723bbbc40ef472cb4561
skydeszka <60075235+Skydeszka@users.noreply.github.com>
2022-05-24 15:45:14 +0200

    Create BasicLogger.cs

---
+++ Loggers/BasicLogger.cs
@@ -0,0 +1,19 @@
+﻿using Discord;
+using System.Text;
+
+namespace TestBot.Loggers;
+
+static class BasicLogger
+{
+    public static string LogToConsole(LogMessage log)
+    {
+        var builder = new StringBuilder("[");
+        builder.Append(DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss"));
+        builder.Append("] ");
+        builder.Append(log.Source);
+        builder.Append(": ");
+        builder.AppendLine(log.Message);
+
+        return builder.ToString();
+    }
+}
