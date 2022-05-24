31fbe0dee0cb6a42ef697341358977345e827b35
skydeszka <60075235+Skydeszka@users.noreply.github.com>
2022-05-24 16:09:08 +0200

    Added Severity Color

--- Loggers/BasicLogger.cs
+++ Loggers/BasicLogger.cs
@@ -14,7 +14,27 @@ static class BasicLogger
         builder.Append(": ");
         builder.Append(log.Message);
 
+        switch (log.Severity)
+        {
+            case LogSeverity.Critical:
+            case LogSeverity.Error:
+                Console.ForegroundColor = ConsoleColor.Red;
+                break;
+            case LogSeverity.Warning:
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                break;
+            case LogSeverity.Info:
+                Console.ForegroundColor = ConsoleColor.White;
+                break;
+            case LogSeverity.Verbose:
+            case LogSeverity.Debug:
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                break;
+        }
+
         Console.WriteLine(builder.ToString());
+
+        Console.ResetColor();
 
         return Task.CompletedTask;
     }
