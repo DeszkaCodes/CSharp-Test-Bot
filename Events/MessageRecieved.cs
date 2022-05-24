6fa3601dba1ec27639307c28226b8b4852bc1313
skydeszka <60075235+Skydeszka@users.noreply.github.com>
2022-05-24 23:31:45 +0200

    Created AddExperience method

--- Events/MessageRecieved.cs
+++ Events/MessageRecieved.cs
@@ -1,14 +1,22 @@
 ﻿using Discord.Commands;
 using Discord.WebSocket;
+using TestBot.Data;
+using TestBot.Models;
+using TestBot.Services;
 
 namespace TestBot.Events;
 
-static class MessageRecieved
+public class MessageRecieved
 {
     public const string DefaultPrefix = "!test ";
 
     public static string[] Prefixes = new string[] { DefaultPrefix };
-    
+
+    private const byte MaxExp = 10;
+    private const byte MinExp = 5;
+
+    private static Random RandomGenerator = new Random();
+
     public static async Task CheckForCommand(SocketMessage rawMessage)
     {
         var message = rawMessage as SocketUserMessage;
@@ -33,5 +41,28 @@ static class MessageRecieved
         
         if(!result.IsSuccess)
             await context.Channel.SendMessageAsync(result.ErrorReason);
+    }
+    
+    public static async Task AddExperience(SocketMessage rawMessage)
+    {
+        var service = Program.Services?.GetService(typeof(UserService)) as UserService;
+        var message = rawMessage as SocketUserMessage;
+
+        if (message is null || service is null)
+            return;
+
+        byte exp = (byte)RandomGenerator.Next(MinExp, MaxExp);
+
+        var user = await service.GetById(message.Author.Id);
+
+        if (user is null)
+            user = await service.AddUser(message.Author.Id);
+
+        await service.AddExperience(user.Id, exp);
+    }
+
+    private static int ExperienceToLevel(uint exp)
+    {
+        return (int)Math.Pow(exp / 42, 0.55);
     }
 }
