7f07246b53d5a3e149f243cf3ebebd84eccd5de2
skydeszka <60075235+Skydeszka@users.noreply.github.com>
2022-05-24 21:56:49 +0200

    Create InfoModule.cs

---
+++ Commands/InfoModule.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Commands;
+
+namespace TestBot.Commands;
+
+public class InfoModule : ModuleBase<SocketCommandContext>
+{
+    [Command("ping", RunMode = RunMode.Async)]
+    [Summary("Returns the latency of the bot")]
+    [RequireContext(ContextType.Guild)]
+    public async Task Ping()
+    {
+        await ReplyAsync($"Latency is: {Context.Client.Latency}ms");
+    }
+}
