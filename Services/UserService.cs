3b711f4ec9501956a97aa4df9342a3554b7c585e
skydeszka <60075235+Skydeszka@users.noreply.github.com>
2022-05-24 23:32:05 +0200

    Create UserService.cs

---
+++ Services/UserService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestBot.Data;
+using TestBot.Models;
+
+namespace TestBot.Services;
+class UserService
+{
+    private readonly BotContext _context;
+    public UserService(BotContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User?> GetById(ulong id)
+    {
+        return await _context.Users
+            .AsNoTracking()
+            .SingleOrDefaultAsync(user => user.Id == id);
+    }
+
+    public async Task<User> AddUser(ulong id)
+    {
+        var user = new User { Id = id };
+        await _context.Users.AddAsync(user);
+        await _context.SaveChangesAsync();
+        return user;
+    }
+
+    public async Task AddExperience(ulong id, byte exp)
+    {
+        var user = await _context.Users.FindAsync(id);
+
+        if (user is null)
+            return;
+
+        user.Experience += exp;
+
+        await _context.SaveChangesAsync();
+    }
+}
