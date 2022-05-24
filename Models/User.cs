b0131c80bd9aa1ac52d42dc197818d4dcc182e46
skydeszka <60075235+Skydeszka@users.noreply.github.com>
2022-05-24 23:32:01 +0200

    Create User.cs

---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿#pragma warning disable CS8618
+// Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace TestBot.Models;
+
+public class User
+{
+    [StringLength(18)]
+    public ulong Id { get; set; }
+
+    [DefaultValue(0)]
+    public uint Experience { get; set; } = 0;
+}
+
+#pragma warning restore CS8618
