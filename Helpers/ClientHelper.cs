b3e3e276d5ad7a4b8690dd223e53ed885feb34ae
skydeszka <60075235+Skydeszka@users.noreply.github.com>
2022-05-24 16:52:33 +0200

    Create ClientHelper.cs

---
+++ Helpers/ClientHelper.cs
@@ -0,0 +1,35 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace TestBot.Handlers;
+
+static class ClientHelper
+{
+    public static async Task StartClient(DiscordSocketClient client, string token)
+    {
+        await client.LoginAsync(TokenType.Bot, token);
+        await client.StartAsync();
+    }
+
+    public static DiscordSocketClient CreateClient(Func<LogMessage, Task>? logger = null)
+    {
+        var client = new DiscordSocketClient();
+
+        if (logger is not null)
+            client.Log += logger;
+
+        return client;
+    }
+
+    public static DiscordSocketClient CreateClient(IEnumerable<Func<LogMessage, Task>> loggers)
+    {
+        var client = new DiscordSocketClient();
+
+        foreach (var logger in loggers)
+        {
+            client.Log += logger;
+        }
+
+        return client;
+    }
+}
