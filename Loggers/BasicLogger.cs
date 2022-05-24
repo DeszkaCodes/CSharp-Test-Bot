fd91542dbd8395e16b44bb1604d03c3a9b8f9508
skydeszka <60075235+Skydeszka@users.noreply.github.com>
2022-05-24 15:51:11 +0200

    Update BasicLogger.cs

--- Loggers/BasicLogger.cs
+++ Loggers/BasicLogger.cs
@@ -5,15 +5,17 @@ namespace TestBot.Loggers;
 
 static class BasicLogger
 {
-    public static string LogToConsole(LogMessage log)
+    public static Task LogToConsole(LogMessage log)
     {
         var builder = new StringBuilder("[");
         builder.Append(DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss"));
         builder.Append("] ");
         builder.Append(log.Source);
         builder.Append(": ");
-        builder.AppendLine(log.Message);
+        builder.Append(log.Message);
 
-        return builder.ToString();
+        Console.WriteLine(builder.ToString());
+
+        return Task.CompletedTask;
     }
 }
